5ee6f496638f5d7844e9fbb344f4d71f765f36a5
Jonathan Comin Ribeiro <jothbc@gmail.com>
2023-04-05 21:39:40 -0300

    Add files via upload

---
+++ Projeto_desenvolver/frontend_marco_2023_ultima_aula/js/routes.js
@@ -0,0 +1,10 @@
+// const baseURL = 'http://localhost:3333'
+const baseURL = 'http://192.168.100.203:3333'
+
+export const API_ROUTES = {
+  LOGIN: `${baseURL}/login`,
+  REGISTER: `${baseURL}/users`,
+  POSTS: `${baseURL}/post`,
+  IMAGE: `${baseURL}/files/`,
+  AVATAR: `${baseURL}/users/avatar`,
+}
