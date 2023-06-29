5e1c3ff8547efdd6f3706d4bbf7ca0f98f4ebfd0
Jonathan Comin Ribeiro <jonathan.ribeiro@grupow.com.br>
2023-06-28 22:02:02 -0300

    removed unnecessary files

--- MW/frontend/js/routes.js
+++
@@ -1,8 +0,0 @@
-const baseURL = 'http://192.168.2.80:3333'
-
-export const API_ROUTES = {
-  LOGIN: `${baseURL}/login`,
-  REGISTER: `${baseURL}/users`,
-  POSTS: `${baseURL}/post`,
-  IMAGE: `${baseURL}/files/`,
-}
